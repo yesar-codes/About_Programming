1a63472420ddfba813b1699fe09dd67a15b629bf
Sarthak Bajaj <udgvq@student.kit.edu>
2023-03-13 14:50:29 +0100

    added

---
+++ Day15Strings/TakingInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class TakingInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input");
+        String m = sc.nextLine();
+        System.out.println("The inputted String is:"+m);
+    }
+}
