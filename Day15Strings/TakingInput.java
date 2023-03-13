0c025e50f14978f10017984969dfb72d160372d6
Sarthak Bajaj <udgvq@student.kit.edu>
2023-03-13 15:17:55 +0100

    added

--- Day15Strings/TakingInput.java
+++ Day15Strings/TakingInput.java
@@ -5,5 +5,20 @@ public class TakingInput {
         System.out.println("Enter the input");
         String m = sc.nextLine();
         System.out.println("The inputted String is:"+m);
+
+        Concatenation obj = new Concatenation();
+        String f = obj.Concatenate();//String after concatenation
+        System.out.println(f);
+
+        LengthOfString obj1 = new LengthOfString();
+        System.out.println(obj1.getLengthOfString("Hello"));//printing the length of hello
+        CompareTwoStrings obj2 = new CompareTwoStrings();
+        System.out.println(obj2.CompareString("hello","hello"));//comparing two strings
+        System.out.println(obj1.getLengthOfString("refrigerator"));//finding the length of refrigerator
+        CheckIfAlphabetPresent obj3 = new CheckIfAlphabetPresent();
+        System.out.println(obj3.CheckIfThere("Umbrella",'e'));
+        /*checking if the word is present in the umbrella string*/
+        DeleteAllConsonants obj4 = new DeleteAllConsonants();
+        System.out.println(obj4.afterDeleting("Hello,have a good day"));
     }
 }
